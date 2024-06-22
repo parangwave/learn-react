0eff5ee16e1690e1a46001a6ffee6015fb13bce8
Oh Jisu <parangwavetraveller@gmail.com>
2024-06-23 02:56:58 +0900

    Effects - Deps

--- effects/src/App.js
+++ effects/src/App.js
@@ -2,7 +2,10 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
 
   // const iRunOnlyOnce = () => {
@@ -10,12 +13,41 @@ function App() {
   // }
   // useEffect(iRunOnlyOnce, []);
 
-  useEffect(()=> {
-    console.log("CALL THE API");
+  // useEffect(() => {
+  //   console.log("CALL THE API");
+  // }, []);
+  // console.log("SEARCH FOR", keyword);
+  
+  // useEffect(() => {
+  //   if (keyword !== "" && keyword.length > 5) {
+  //     console.log("SEARCH FOR", keyword);
+  //   }
+  // }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run only once.");
   }, []);
+
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.")
+  }, [keyword]);
+  
+  useEffect(() => {
+    console.log("I run when 'counter' changes.")
+  }, [counter]);
+  
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.")
+  }, [keyword, counter]);
 
   return (
     <div className="App">
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
