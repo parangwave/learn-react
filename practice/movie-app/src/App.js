4edd03f0217471148a4dd62a3be1ce3dd9c77d55
Oh Jisu <parangwavetraveller@gmail.com>
2024-06-24 02:57:47 +0900

    Practice - Movie App pt.1

---
+++ practice/movie-app/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const resp = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    );
+    const json = await resp.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} alt="movie.medium_cover_image" />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default App;
